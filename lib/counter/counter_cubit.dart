93efc0a79a22c1123628eb5fb5a1d0c768c86a2d
NM71 <noushermurtaza456@gmail.com>
2024-10-15 10:37:08 +0500

    OnChange for state change

--- lib/counter/counter_cubit.dart
+++ lib/counter/counter_cubit.dart
@@ -17,4 +17,14 @@ class CounterCubit extends Cubit<int>{
 
   //decrement
   void decrement() => emit(state - 1);
+
+  // on state statechange -> do something
+  @override
+  void onChange(Change<int> change) {
+    super.onChange(change);
+
+    // prints the change in state in Console
+    print(change);
+  }
+
 }
